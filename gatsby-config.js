785255fce17c43e3a488c3aec8c947042ebdeb6f
Michael Beale <michael.beale@gmail.com>
2023-01-08 14:56:54 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
       options: {
         name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
         short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        description: `I'm Michael - A Customer Experience professional based out of Holly Springs, NC. Currently I'm a Technical Support Manager at SolarWinds, Inc.`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
